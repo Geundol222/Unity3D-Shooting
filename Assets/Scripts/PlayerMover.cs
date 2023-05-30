c006c5125ce115d7b7994b96331be0bedaf3b82e
Geundol222 <geundol222@gmail.com>
2023-05-30 17:01:31 +0900

    feat : Player Move

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+
+    private CharacterController controller;
+    private Vector3 moveDir;
+    private float ySpeed = 0f;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveDir = new Vector3(input.x, 0, input.y);
+    }
+
+    private void Jump()
+    {
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (GroundCheck() && ySpeed < 0)
+            ySpeed = 0f;
+
+        controller.Move(Vector3.up * ySpeed * Time.deltaTime);
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (GroundCheck())
+            ySpeed = jumpSpeed;
+    }
+
+    private bool GroundCheck()
+    {
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position + Vector3.up * 1, 0.5f, Vector3.down, out hit, 0.6f);
     }
 }
