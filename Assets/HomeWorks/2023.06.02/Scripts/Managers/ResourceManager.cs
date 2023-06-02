7fbce0f615d6d3f5695d29b01306fdf4660bd58b
Geundol222 <geundol222@gmail.com>
2023-06-02 21:48:47 +0900

    feat : HomeWork 0602

--- Assets/HomeWorks/2023.06.02/Scripts/Managers/ResourceManager.cs
+++ Assets/HomeWorks/2023.06.02/Scripts/Managers/ResourceManager.cs
@@ -6,16 +6,40 @@ namespace HomeWork0602
 {
     public class ResourceManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        Dictionary<string, Object> resources = new Dictionary<string, Object>();
 
+        public T Load<T>(string path) where T : Object
+        {
+            string key = $"{typeof(T)}.{path}";
+
+            if (resources.ContainsKey(key))
+                return resources[key] as T;
+
+            T resource = Resources.Load<T>(path);
+            resources.Add(key, resource);
+            return resource;
         }
 
-        // Update is called once per frame
-        void Update()
+        public T Instantiate<T>(T original, Vector3 position, Quaternion rotation, Transform parent, bool pooling = false) where T : Object
         {
+            if (pooling)
+                return GameManager.Pool.Get(original, position, rotation, parent);
+            else
+                return Object.Instantiate(original, position, rotation, parent);
+        }
 
+        public T Instantiate<T>(string path, Vector3 position, Quaternion rotation, Transform parent, bool pooling = false) where T : Object
+        {
+            T original = Load<T>(path);
+            return Instantiate<T>(original, position, rotation, parent, pooling);
+        }
+
+        public void Destroy(GameObject obj)
+        {
+            if (GameManager.Pool.Release(obj))
+                return;
+
+            GameObject.Destroy(obj);
         }
     }
 }
